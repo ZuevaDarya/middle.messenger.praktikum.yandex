b4fd2ff64f0e6c666de8dac7ffcc1602cf1641c2
ZuevaDarya <zuevadasha55@gmail.com>
2024-03-07 17:28:10 +0300

    написан сервер на express

---
+++ server.cjs
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+const PORT = 3000;
+const pathName = `${__dirname}/dist/index.html`;
+
+app.use(express.static(__dirname + 'dist'));
+
+app.get('/*', (req, res) => {
+  res.sendFile(pathName);
+});
+
+app.listen(PORT, () => console.log(`Server on port ${PORT} was started!`));
