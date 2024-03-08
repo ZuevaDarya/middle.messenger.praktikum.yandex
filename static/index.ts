7ecaa9aa286bb385616d72121a2856cf1aea9f9c
ZuevaDarya <zuevadasha55@gmail.com>
2024-03-08 19:14:13 +0300

    написала функцию для навигации

---
+++ static/index.ts
@@ -0,0 +1,31 @@
+import * as Pages from '../src/pages';
+import Handlebars from 'handlebars';
+
+type Page = {
+  [key: string] : unknown[];
+}
+
+const pages: Page = {
+  'login': [Pages.Login]
+};
+
+function navigate(page: string) {
+  const [source, args] = pages[page];
+  const handlebarsFunct = Handlebars.compile(source);
+
+  document.body.innerHTML = handlebarsFunct(args);
+}
+
+document.addEventListener('DOMContentLoaded', () => navigate('login'));
+
+document.addEventListener('click', (e: MouseEvent) => {
+  const page = (e.target as Element)?.getAttribute('page');
+
+  if (page) {
+    navigate(page);
+
+    e.preventDefault();
+    e.stopImmediatePropagation();
+  }
+});
+
