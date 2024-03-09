44f281c141b3cc45e7383d0eeb2dee8bb24b523e
ZuevaDarya <zuevadasha55@gmail.com>
2024-03-09 14:21:42 +0300

    экспортировала компоненты

---
+++ src/components/index.ts
@@ -0,0 +1,5 @@
+export { Input } from './input';
+export { InputField } from './input-field';
+export { Button } from './button';
+export { Link } from './link';
+export { FormTitle } from './form-title';
