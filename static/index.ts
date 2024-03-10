365313fc95c12e3716a3f45c7c2bae58e9e6ac21
ZuevaDarya <zuevadasha55@gmail.com>
2024-03-10 22:47:36 +0300

    добавила новую страницу в навигацию

--- static/index.ts
+++ static/index.ts
@@ -8,7 +8,8 @@ type Page = {
 
 const pages: Page = {
   'login': [Pages.Login],
-  'registration': [Pages.Registration]
+  'registration': [Pages.Registration],
+  'chat': [Pages.Chat],
 };
 
 Object.entries(Components).forEach(([name, component]) => {
@@ -35,3 +36,11 @@ document.addEventListener('click', (e: MouseEvent) => {
   }
 });
 
+Handlebars.registerHelper('chat-list', () => [
+    { name: 'Иван Иванов', senderName: 'Вы', message: 'стикер', time: '12:30'},
+    { name: 'Макс', senderName: 'Вы', message: 'До завтра!', time: '17:00' },
+    { name: 'Киноклуб', senderName: 'Вы', message: 'стикер', time: '21:05'},
+    { name: 'Елена Половинкина', message: 'Можно на сегодня или завтра. Ты как?', time: 'Пн', numMessage: '3' },
+    { name: 'Анна Блая', message: 'Изображение', time: 'Пн', numMessage: '2' },
+    { name: 'Design Destroyer', message: 'В 2008 году художник Jon Rafman  начал собирать...', time: '1 Мая 2020'}
+]);
