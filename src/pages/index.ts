a643f74aed731a0cc56d02120f77c9e55a77ee06
ZuevaDarya <zuevadasha55@gmail.com>
2024-03-11 23:45:45 +0300

    экспортировала новые страницы

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,3 +1,6 @@
 export { Login } from './login';
 export { Registration } from './registration';
 export { Chat } from './chat';
+export { Profile } from './profile';
+export { ProfileData } from './profile-data';
+export { ProfilePassword } from './profile-password';
