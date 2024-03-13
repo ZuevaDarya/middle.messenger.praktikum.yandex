4af2d7c0b8fe8a2e37fb9391bfd46fa433869664
ZuevaDarya <zuevadasha55@gmail.com>
2024-03-13 18:38:17 +0300

    рефакторинг кода

--- server.cjs
+++ server.cjs
@@ -2,13 +2,12 @@ const express = require('express');
 
 const app = express();
 const PORT = 3000;
-const pathName = `${__dirname}/dist/index.html`;
 
 app.use(express.static(__dirname + '/dist'));
 
 app.get('/', (req, res) => {
   res.status(200);
-  res.sendFile(pathName);
+  res.send('Messenger');
 });
 
 app.listen(PORT, () => console.log(`Server on port ${PORT} was started!`));
