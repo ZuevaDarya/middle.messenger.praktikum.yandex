7ba7de773c7071bae5af7ebb5e535fc47a27259f
ZuevaDarya <zuevadasha55@gmail.com>
2024-03-13 18:34:20 +0300

    рефакторинг кода

--- server.cjs
+++ server.cjs
@@ -6,7 +6,7 @@ const pathName = `${__dirname}/dist/index.html`;
 
 app.use(express.static(__dirname + '/dist'));
 
-app.get('/*', (req, res) => {
+app.get('/', (req, res) => {
   res.status(200);
   res.sendFile(pathName);
 });
