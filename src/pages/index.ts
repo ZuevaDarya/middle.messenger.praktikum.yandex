69062b3ad257c612a7087f004a68fec2ea2b6284
ZuevaDarya <zuevadasha55@gmail.com>
2024-03-13 15:27:43 +0300

    экспортировала новые страницы

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -4,3 +4,5 @@ export { Chat } from './chat';
 export { Profile } from './profile';
 export { ProfileData } from './profile-data';
 export { ProfilePassword } from './profile-password';
+export { Error400 } from './error-400';
+export { Error500 } from './error-500';
