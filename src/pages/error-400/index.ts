31321a51e731c9b2ef06372b484bdaa930955a5c
ZuevaDarya <zuevadasha55@gmail.com>
2024-03-13 18:23:55 +0300

    правка стилей файлов

--- src/pages/error-400/index.ts
+++ src/pages/error-400/index.ts
@@ -1,2 +1,1 @@
-import './error-400.scss';
 export { default as Error400 } from './error-400.hbs?raw';
--- src/pages/error-500/index.ts
+++ src/pages/error-500/index.ts
@@ -1,2 +1,1 @@
-import './error-500.scss';
 export { default as Error500 } from './error-500.hbs?raw';
