4a7eb64acde5bb3b5493fec249c7d2999c673a22
ZuevaDarya <zuevadasha55@gmail.com>
2024-03-24 22:10:11 +0300

    добавила класс ChatLeftFunctions

--- src/components/chat-left-functions/index.ts
+++ src/components/chat-left-functions/index.ts
@@ -1,2 +1,21 @@
-import './chat-left-functions.scss';
-export { default as ChatLeftFunctions } from './chat-left-functions.hbs?raw';
+
+import Block from '../../shared/utils/block';
+import chatLeftFunctionsTmpl from './chat-left-functions';
+import { Props } from '../../shared/types';
+
+export default class ChatLeftFunctions extends Block {
+  constructor(tagName = 'div', props: Props) {
+    super(
+      tagName,
+      {
+        ...props
+      }
+    )
+  }
+
+  redefineRender() {
+    return chatLeftFunctionsTmpl;
+  }
+}
+
+
