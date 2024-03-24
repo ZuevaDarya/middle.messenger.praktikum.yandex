e6149ba0d7e86ce5441d6570b126bd909baed271
ZuevaDarya <zuevadasha55@gmail.com>
2024-03-24 22:10:26 +0300

    добавила класс ChatList

--- src/components/chat-list/index.ts
+++ src/components/chat-list/index.ts
@@ -1,2 +1,19 @@
-import './chat-list.scss';
-export { default as ChatList } from './chat-list.hbs?raw';
+import Block from '../../shared/utils/block';
+import chatListTmpl from './chat-list';
+import { Props } from '../../shared/types';
+
+export default class ChatList extends Block {
+  constructor(tagName = 'div', props: Props) {
+    super(
+      tagName,
+      {
+        ...props
+      }
+    )
+  }
+
+  redefineRender() {
+    return chatListTmpl;
+  }
+}
+
