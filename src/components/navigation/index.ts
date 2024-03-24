3c6782c7e38c0a6e079a669707961ee758f5886a
ZuevaDarya <zuevadasha55@gmail.com>
2024-03-24 22:14:19 +0300

    добавила класс Navigation

--- src/components/navigation/index.ts
+++ src/components/navigation/index.ts
@@ -1,2 +1,8 @@
-import './navigation.scss';
-export { default as Navigation } from './navigation.hbs?raw';
+import Block from '../../shared/utils/block';
+import navigationTmpl from './navigation';
+
+export default class Navigation extends Block {
+  redefineRender() {
+    return navigationTmpl;
+  }
+}
---
+++ src/components/navigation/navigation.ts
@@ -0,0 +1,9 @@
+import './navigation.scss';
+
+const navigationTmpl = `
+  <nav class="navigation">
+  {{{ lists }}}
+  </nav>
+`;
+
+export default navigationTmpl;
