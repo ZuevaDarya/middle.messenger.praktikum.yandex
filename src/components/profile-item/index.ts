55ab47113314fd03c8c08c839e9bd23dbc1757aa
ZuevaDarya <zuevadasha55@gmail.com>
2024-03-24 22:15:45 +0300

    добавила класс ProfileItem

--- src/components/profile-item/index.ts
+++ src/components/profile-item/index.ts
@@ -1,2 +1,19 @@
-import './profile-item.scss';
-export { default as ProfileItem } from './profile-item.hbs?raw';
+import Block from '../../shared/utils/block';
+import profileItemTmpl from './profile-item';
+import { Props } from '../../shared/types';
+
+export default class profileItem extends Block {
+  constructor(tagName = 'div', props: Props) {
+    super(
+      tagName,
+      {
+        ...props
+      }
+    )
+  }
+
+  redefineRender() {
+    return profileItemTmpl;
+  }
+}
+
--- src/components/profile-item/profile-item.ts
+++ src/components/profile-item/profile-item.ts
@@ -1,10 +1,15 @@
+import './profile-item.scss';
 
+const profileItemTmpl = `
 <div 
   class="{{#if className}} {{className}} {{/if}} profile-item"
   page="{{page}}"
->
+  >
   <span class="profile-item__title">{{title}}</span>
   {{#if data}}
     <span class="profile-item__data">{{data}}</span>
   {{/if}}
 </div>
+`;
+
+export default profileItemTmpl;
