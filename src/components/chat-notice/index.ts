5c9adca088475114c1abe89827e6fa8bf5d499ac
ZuevaDarya <zuevadasha55@gmail.com>
2024-03-24 22:11:25 +0300

    добавила класс ChatNotice

--- src/components/chat-notice/index.ts
+++ src/components/chat-notice/index.ts
@@ -1,2 +1,19 @@
-import './chat-notice.scss';
-export { default as ChatNotice } from './chat-notice.hbs?raw';
+import Block from '../../shared/utils/block';
+import chatNoticeTmpl from './chat-notice';
+import { Props } from '../../shared/types';
+
+export default class ChatNotice extends Block {
+  constructor(tagName = 'div', props: Props) {
+    super(
+      tagName,
+      {
+        ...props
+      }
+    )
+  }
+
+  redefineRender() {
+    return chatNoticeTmpl;
+  }
+}
+
