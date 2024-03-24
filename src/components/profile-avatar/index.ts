83eb7bc32f46a6b50aade29f3daf055c9fa1eee5
ZuevaDarya <zuevadasha55@gmail.com>
2024-03-24 22:14:47 +0300

    добавила класс profileAvatar

--- src/components/profile-avatar/index.ts
+++ src/components/profile-avatar/index.ts
@@ -1,2 +1,19 @@
-import './profile-avatar.scss'
-export { default as ProfileAvatar } from './profile-avatar.hbs?raw';
+import Block from '../../shared/utils/block';
+import profileAvatarTmpl from './profile-avatar';
+import { Props } from '../../shared/types';
+
+export default class profileAvatar extends Block {
+  constructor(tagName = 'div', props: Props) {
+    super(
+      tagName,
+      {
+        ...props
+      }
+    )
+  }
+
+  redefineRender() {
+    return profileAvatarTmpl;
+  }
+}
+
