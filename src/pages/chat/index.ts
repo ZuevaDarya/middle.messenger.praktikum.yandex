1231897ba08407b50f31639b43f1e2e7f63bc515
ZuevaDarya <zuevadasha55@gmail.com>
2024-03-25 16:25:42 +0300

    добавила класс Chat

--- src/pages/chat/index.ts
+++ src/pages/chat/index.ts
@@ -1,12 +1,90 @@
-import './chat.scss';
-import Handlebars from 'handlebars';
-export { default as Chat } from './chat.hbs?raw';
+import Avatar from '../../components/avatar';
+import Chat from './chat';
+import ChatButton from '../../components/chat-button';
+import ChatHeaderLink from '../../components/chat-header-link';
+import ChatLeftFunctions from '../../components/chat-left-functions';
+import ChatList from '../../components/chat-list';
+import ChatListItem from '../../components/chat-list-item';
+import ChatListItemMessage from '../../components/chat-list-item-message';
+import ChatSendMessageBlock from '../../components/chat-send-message-block';
+import ChatTitle from '../../components/chat-title';
+import Link from '../../components/link';
+import Navigation from '../../components/navigation';
 
-Handlebars.registerHelper('chat-list', () => [
-  { name: 'Иван Иванов', senderName: 'Вы', message: 'стикер', time: '12:30' },
-  { name: 'Макс', senderName: 'Вы', message: 'До завтра!', time: '17:00' },
-  { name: 'Киноклуб', senderName: 'Вы', message: 'стикер', time: '21:05' },
-  { name: 'Елена Половинкина', message: 'Можно на сегодня или завтра. Ты как?', time: 'Пн', numMessage: '3' },
-  { name: 'Анна Блая', message: 'Изображение', time: 'Пн', numMessage: '2' },
-  { name: 'Design Destroyer', message: 'В 2008 году художник Jon Rafman  начал собирать...', time: '1 Мая 2020' }
-]);
+export const chatPage = new Chat('div', {
+  navigation: new Navigation('nav', {
+    lists: [
+      new Link('a', { url: '', text: 'Логин', page: 'login' }),
+      new Link('a', { url: '', text: 'Регистрация', page: 'registration' }),
+      new Link('a', { url: '', text: 'Чат', page: 'chat' }),
+      new Link('a', { url: '', text: 'Профиль', page: 'profile' }),
+      new Link('a', { url: '', text: 'Изменить данные профиля', page: 'profileData' }),
+      new Link('a', { url: '', text: 'Изменить пароль профиля', page: 'ProfilePassword' }),
+      new Link('a', { url: '', text: 'Ошибка 404', page: 'error400' }),
+      new Link('a', { url: '', text: 'Ошибка 500', page: 'error500' }),
+    ]
+  }),
+  avatar: new Avatar('div', {
+    src: '/img/avatar.png',
+    alt: 'Аватар профиля пользователя'
+  }),
+  chatHeaderLink: new ChatHeaderLink('a', {
+    text: 'Профиль',
+    page: 'profile',
+    src: ''
+  }),
+  chatLeftFunctions: new ChatLeftFunctions(),
+  chatList: new ChatList('div', {
+    chatListItems: [
+      new ChatListItem('div', {
+        time: '12:30',
+        avatar: new Avatar('div', { src: '/img/avatar.png', alt: 'Аватар чата'}),
+        chatTitle: new ChatTitle('p', { name: 'Иван Иванов' }),
+        chatListMessage: new ChatListItemMessage('p', { senderName: 'Вы', message: 'стикер' }),
+      }),
+      new ChatListItem('div', {
+        time: '17:00',
+        avatar: new Avatar('div', { src: '/img/avatar.png', alt: 'Аватар чата'}),
+        chatTitle: new ChatTitle('p', { name: 'Макс' }),
+        chatListMessage: new ChatListItemMessage('p', { senderName: 'Вы', message: 'До завтра!' }),
+      }),
+      new ChatListItem('div', {
+        time: '21:05',
+        avatar: new Avatar('div', { src: '/img/avatar.png', alt: 'Аватар чата'}),
+        chatTitle: new ChatTitle('p', { name: 'Киноклуб' }),
+        chatListMessage: new ChatListItemMessage('p', { senderName: 'Вы', message: 'стикер' }),
+      }),
+      new ChatListItem('div', {
+        time: 'Пн',
+        avatar: new Avatar('div', { src: '/img/avatar.png', alt: 'Аватар чата'}),
+        chatTitle: new ChatTitle('p', { name: 'Елена Половинкина' }),
+        chatListMessage: new ChatListItemMessage('p', { message: 'Можно на сегодня или завтра. Ты как?' }),
+        numMessage: '3'
+      }),
+      new ChatListItem('div', {
+        time: 'Пн',
+        avatar: new Avatar('div', { src: '/img/avatar.png', alt: 'Аватар чата'}),
+        chatTitle: new ChatTitle('p', { name: 'Анна Блая' }),
+        chatListMessage: new ChatListItemMessage('p', { message: 'Изображение' }),
+        numMessage: '2'
+      }),
+      new ChatListItem('div', {
+        time: '1 Мая 2020',
+        avatar: new Avatar('div', { src: '/img/avatar.png', alt: 'Аватар чата'}),
+        chatTitle: new ChatTitle('p', { name: 'Design Destroyer' }),
+        chatListMessage: new ChatListItemMessage('p', {
+          message: 'В 2008 году художник Jon Rafman начал собирать...'
+        }),
+      }),
+    ],
+  }),
+  chatAvatar: new Avatar('div', {
+    src: '/img/avatar.png',
+    alt: 'Аватар чата'
+  }),
+  chatTitle: new ChatTitle('p', { name: 'Иван Иванов' }),
+  chatSendMessageBlock: new ChatSendMessageBlock('div', {
+    chatButtonAddFile: new ChatButton('button', { attr: { class: 'chat-button__add-file-btn' }}),
+    chatButtonSendMsg: new ChatButton('button', { attr: { class: 'chat-button__send-message-btn' }})
+  })
+});
