4583b6e4829876afdc348b83b8ed76858c06d549
ZuevaDarya <zuevadasha55@gmail.com>
2024-03-27 19:59:38 +0300

    добавила класс SearchInput

--- src/components/chat-send-message-block/chat-send-mesage-block.ts
+++ src/components/chat-send-message-block/chat-send-mesage-block.ts
@@ -1,11 +1,11 @@
 import './chat-send-message-block.scss';
 
 const chatSendMessageBlockTmpl = `
-  <div class="chat-send-message">
+  <form class="chat-send-message">
     {{{ chatButtonAddFile }}}
-    <input type="text" name="message" class="chat-send-message__input" placeholder="Сообщение">
+    {{{ searchInput }}}
     {{{ chatButtonSendMsg }}}
-  </div>
+  </form>
 `;
 
 export default chatSendMessageBlockTmpl;
