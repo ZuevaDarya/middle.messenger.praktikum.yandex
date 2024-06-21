3ffb801228590187526b21c13b2c33a4537a7029
ZuevaDarya <zuevadasha55@gmail.com>
2024-06-21 14:55:34 +0300

    добавила утилиты

--- src/shared/utils/query-stringify.ts
+++ src/shared/utils/query-stringify.ts
@@ -1,11 +1,34 @@
-export function queryStringify(data: Record<string, unknown>): string {
-  if (typeof data !== 'object') {
-    throw new Error('Data must be object');
+import isArrayOrObject from './is-array-object';
+import isPlainObject from './is-plain-object';
+import { PlainObject } from '../types';
+
+type StringIndexed = Record<string, unknown>;
+
+function queryStringify(data: StringIndexed): string {
+  if (!isPlainObject(data)) {
+    throw new Error('input must be an object');
   }
 
-  const keys = Object.keys(data);
-
-  return keys.reduce((result, key, index) => (
-    `${result}${key}=${data[key]}${index < keys.length - 1 ? '&' : ''}`
-  ), '?');
+  return getParams(data).map(arr => arr.join('=')).join('&');
 }
+
+function getParams(data: PlainObject | [], parentKey?: string) {
+  const result: [string, string][] = [];
+
+  for (const [key, value] of Object.entries(data)) {
+    if (isArrayOrObject(value)) {
+      result.push(...getParams(value, getKey(key, parentKey)));
+    } else {
+      result.push([getKey(key, parentKey), encodeURIComponent(String(value))]);
+    }
+  }
+
+  return result;
+}
+
+function getKey(key: string, parentKey?: string) {
+  return parentKey ? `${parentKey}: ${key}` : key;
+}
+
+export default queryStringify;
+
