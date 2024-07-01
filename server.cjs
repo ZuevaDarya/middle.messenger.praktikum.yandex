65d71f06156569dcc2d6dba833101faa744f38ad
ZuevaDarya <zuevadasha55@gmail.com>
2024-07-01 14:13:31 +0300

    поправила выдачу файлов после сборки

--- server.cjs
+++ server.cjs
@@ -1,13 +1,16 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 const PORT = 3000;
 
+const pathName = path.join(__dirname, '/dist/index.html');
+
 app.use(express.static(__dirname + '/dist'));
 
-app.get('/', (req, res) => {
+app.get('/*', (req, res) => {
   res.status(200);
-  res.send('Messenger');
+  res.sendFile(pathName);
 });
 
 app.listen(PORT, () => console.log(`Server on port ${PORT} was started!`));
