6e8c0f462217ee6eea11d1220b8020bd17c4ea94
ZuevaDarya <zuevadasha55@gmail.com>
2024-07-01 13:54:43 +0300

    изменила названия роутов

--- src/shared/consts/routes.ts
+++ src/shared/consts/routes.ts
@@ -1,8 +1,8 @@
 export enum Routes {
   Login = '/',
   Registration = '/sign-up',
-  Chats = '/chats',
-  Profile = '/profile',
+  Chats = '/messenger',
+  Profile = '/settings',
   ProfileData = '/profile-data',
   ProfilePassword = '/profile-password',
   Error400 = '/error-400',
