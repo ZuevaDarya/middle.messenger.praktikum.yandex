1f280d6287f518f3faf3871eff9f09f1bf95ddd0
ZuevaDarya <zuevadasha55@gmail.com>
2024-07-05 15:33:45 +0300

    добавила мок на localStorage

--- mocha-setup.mjs
+++ mocha-setup.mjs
@@ -1,12 +1,16 @@
 import { JSDOM } from 'jsdom';
+import 'mock-local-storage';
 import sinon from 'sinon';
 
 const jsdom = new JSDOM('<main></main>', {
   url: 'http://localhost:3000',
 });
 
-global.window = jsdom.window;
 global.document = jsdom.document;
 global.DocumentFragment = jsdom.window.DocumentFragment;
+global.window = jsdom.window;
+Object.defineProperty(window, 'localStorage', {
+  value: global.localStorage
+});
 
 global.XMLHttpRequest = sinon.useFakeXMLHttpRequest();
